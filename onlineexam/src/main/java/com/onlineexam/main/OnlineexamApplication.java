75e3ddb60862c92f1be9871863a781e9f1c76ed6
Andrews <andrewsselvaraj@gmail.com>
2022-05-14 01:51:17 +0530

    Base version of Online exam

---
+++ onlineexam/src/main/java/com/onlineexam/main/OnlineexamApplication.java
@@ -0,0 +1,13 @@
+package com.onlineexam.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineexamApplication.class, args);
+	}
+
+}
