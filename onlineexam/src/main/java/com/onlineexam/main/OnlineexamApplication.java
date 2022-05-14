f6ee45513ec4542afc16299ef602e1a14474120b
Andrews <andrewsselvaraj@gmail.com>
2022-05-14 09:29:20 +0530

    New Restructure Code new2

--- onlineexam/src/main/java/com/onlineexam/main/OnlineexamApplication.java
+++ onlineexam/src/main/java/com/onlineexam/main/OnlineexamApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OnlineexamApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(OnlineexamApplication.class, args);
 	}
 
