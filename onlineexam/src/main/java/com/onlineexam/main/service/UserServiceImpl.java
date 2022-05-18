8f59fb7f5e9d0d045038a0d1e4c7bf03f4b9382b
Andrews <andrewsselvaraj@gmail.com>
2022-05-18 14:00:06 +0530

    Online Exam

--- onlineexam/src/main/java/com/onlineexam/main/service/UserServiceImpl.java
+++ onlineexam/src/main/java/com/onlineexam/main/service/UserServiceImpl.java
@@ -11,8 +11,8 @@ import com.onlineexam.main.repository.UserRepository;
 @Service
 public class UserServiceImpl implements UserService{
 
-//	@Autowired
-//    private UserRepository userRepository;
+	@Autowired
+    private UserRepository userRepository;
 	@Override
 	public List<User_details> fetchUserDetailsList() {
 		// TODO Auto-generated method stub
@@ -20,7 +20,8 @@ public class UserServiceImpl implements UserService{
 		User_details us = new User_details();
 		ArrayList<User_details> al = new ArrayList<User_details>();
 		al.add(us);
-		return al;
+		//return al;
+		return userRepository.findAll();
 	}
 
 }
