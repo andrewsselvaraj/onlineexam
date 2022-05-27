857b37560778b097d8b9cf94cacc1cc62a042c73
DELL <DELL@DESKTOP-U6FD22T>
2022-05-27 14:02:32 +0530

    base version

--- onlineexam/src/main/java/com/onlineexam/main/service/UserService.java
+++ onlineexam/src/main/java/com/onlineexam/main/service/UserService.java
@@ -8,6 +8,8 @@ import com.onlineexam.main.model.User_details;
 
 public interface UserService {
 	List<User_details> fetchUserDetailsList();
+	List<User_details> fetchAllUserDetailsList();
 	User_details findByUser_nameAndUser_password(String userName,String password);
 	HashMap<String, String> getTokenAfterLogin(String userName,String password);
 }
+
